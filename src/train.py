a64e9bd73efeea00d3078c9d6acafd5f13f435f0
etienne <etienne.bennequin@gmail.com>
2021-06-01 16:00:46 +0200

    Add first training script

---
+++ src/train.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+import torch
+from torch import nn
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from torchvision.models import resnet18
+
+from easyfsl.data_tools import EasySet, TaskSampler
+from easyfsl.methods import PrototypicalNetworks
+
+train_set = EasySet(specs_file="./data/CUB/train.json", training=True)
+train_sampler = TaskSampler(
+    train_set, n_way=5, n_shot=5, n_query=10, n_tasks=20
+)
+train_loader = DataLoader(
+    train_set,
+    batch_sampler=train_sampler,
+    num_workers=12,
+    pin_memory=True,
+    collate_fn=train_sampler.episodic_collate_fn,
+)
+
+convolutional_network = resnet18(pretrained=False)
+convolutional_network.fc = nn.Flatten()
+model = PrototypicalNetworks(convolutional_network).cuda()
+
+optimizer = Adam(params=model.parameters())
+
+model.fit(train_loader, optimizer)
+
+torch.save(model.state_dict(), Path("./data/models/trained_model.tar"))
