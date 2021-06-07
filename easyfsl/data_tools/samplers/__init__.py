432ef7305d73eaea1a4acc03ae647778ae6dc9ba
etienne <etienne.bennequin@gmail.com>
2021-06-07 14:14:18 +0200

    Add semantic sampler

--- easyfsl/data_tools/samplers/__init__.py
+++ easyfsl/data_tools/samplers/__init__.py
@@ -1,3 +1,4 @@
 from .abstract_task_sampler import AbstractTaskSampler
 from .adaptive_task_sampler import AdaptiveTaskSampler
+from .semantic_task_sampler import SemanticTaskSampler
 from .uniform_task_sampler import UniformTaskSampler
