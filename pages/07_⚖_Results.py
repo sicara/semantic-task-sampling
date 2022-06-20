4d2103b1af7fab49393c13b6203feec1a188cdd9
ebennequin <etienne.bennequin@gmail.com>
2022-06-20 14:44:03 -0400

    Add results for uniform sampling benchmark

--- pages/07_⚖_Results.py
+++ pages/07_⚖_Results.py
@@ -3,33 +3,18 @@ import streamlit as st
 from matplotlib import pyplot as plt
 import seaborn as sns
 
-from st_scripts.st_utils.data_fetchers import (
-    get_testbed,
-    get_easyset_expo,
-    get_class_names,
-    build_task_coarsities_df,
-)
-from st_scripts.st_utils.plot_helpers import plot_coarsities_hist, plot_task, render_svg
-from st_scripts.st_utils.st_app_blocks import (
-    draw_semantic_task,
-)
+from st_scripts.st_utils.plot_helpers import render_svg
 
 from st_scripts.st_utils.st_constants import (
-    TIERED_TEST_SPECS_FILE,
-    TESTBEDS_ROOT_DIR,
     set_slide_page,
-    SEMANTIC_SLIDER_STEP,
-    S3_ROOT_TIERED,
     navigation_buttons,
-    PRIMARY_APP_COLOR,
 )
-from st_scripts.st_utils.st_wordings import st_divider
 
 set_slide_page()
 
 key = 7
 
-navigation_buttons(key, n_steps=1)
+navigation_buttons(key, n_steps=2)
 
 render_svg(f"slides_images/07_{st.session_state.intra_slide_step[key]}.svg")
 
